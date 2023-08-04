7e84b12e0670c21fe745c7e00b361bd700da9a58
sakurawaifu <2606843065@qq.com>
2023-08-04 10:23:38 +0800

    feat: 增加 @ 路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import autoprefixer from 'autoprefixer'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   server: {
     open: true
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
   },
   css: {
     modules: {
